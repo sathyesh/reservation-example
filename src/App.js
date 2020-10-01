8ef1943f5899e64e3ae81e921a61224e1170a7da
Sathyesh <sathyesh.fns@gmail.com>
2020-10-01 10:37:44 +0200

    feat: Added theme support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./Styles/App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ImageHolder from "./components/ImageHolder/ImageHolder";
@@ -14,127 +13,150 @@ import Alert from "@material-ui/lab/Alert";
 import useFetch from "./hooks/useFetch";
 import { restaurantApi } from "./services/api";
 import LanguageToggle from "./components/LanguageToggle";
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
+
+import "./Styles/App.scss";
 
 function App() {
   const [language, setLanguage] = useState("de");
+  const [restaurantTheme, setRestaurantTheme] = useState({
+    primaryColor: '#d0d0d0', // default Value on load replace this with colorPalette
+    contrastText: '#000000' // default Value
+  });
+  
   const restaurantID = "schillingroofbar";
   const [response, isLoading, error] = useFetch({
     api: restaurantApi,
     method: "get",
     url: restaurantID,
   });
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     if (response !== null) {
-      console.log(response);
       setData(response);
     }
   }, [response]);
 
+  useEffect(() => {
+    if (data !== "undefined" && data.colorPalette) {
+      setRestaurantTheme(data.colorPalette);
+    }
+  }, [data]);
+
 
   return (
-    <>
-      {isLoading && <CircularProgress />}
-      {error && error.message != null && (
-        <Alert severity="error">{error.message}</Alert>
-      )}
-      <Container maxWidth="sm">
-        <Typography component="div" className="App">
-          {/* Header Section Start */}
-          <div className="App__Header">
-            <Header
-              link={data.link}
-              logo={data.logo}
-            >
-              <LanguageToggle 
-                language={language}
-                setLanguage={setLanguage}
-               />
-            </Header>
-          </div>
-          {/* Header Section End */}
-          {/* Content Section Start*/}
-          <div className="App__ImageHolder">
-            <ImageHolder src={data.image} />
-          </div>
-          <div className="App__Grid">
-            <Grid container spacing={0}>
-              <Grid item xs={4}>
-                Grid1Grid1Grid1Grid1Grid1Grid1Grid1Grid1
-              </Grid>
-              <Grid item xs={4}>
-                Grid1
-              </Grid>
-              <Grid item xs={4} style={{ textAlign: "center" }}>
-                <Button variant="outlined" color="primary">
-                  Primary
-                </Button>
-              </Grid>
-            </Grid>
-            <div>...</div>
-            <Grid container spacing={0}>
-              <Grid item xs={4}>
-                Grid1
-              </Grid>
-              <Grid item xs={4}>
-                Grid1
-              </Grid>
-              <Grid item xs={4} style={{ textAlign: "center" }}>
-                <Button variant="contained" color="primary">
-                  Primary
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="App__Button">
-            <Grid container spacing={0}>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ minWidth: "100%" }}
-                >
-                  Primary
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="App__Button">
-            <Grid container spacing={0}>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ minWidth: "100%" }}
-                >
-                  Primary
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="App__Button">
-            <Grid container spacing={0}>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ minWidth: "100%" }}
-                >
-                  Primary
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-          {/* Content Section End*/}
-          {/* Footer Section Start */}
-          <div className="App__Footer">
-            <Footer />
-          </div>
-          {/* Footer Section End */}
-        </Typography>
-      </Container>
-    </>
+    <ThemeProvider theme={theme(restaurantTheme)}>
+       {
+       error && error.message != null && (
+              <Alert severity="error">{error.message}</Alert>
+            )
+       }
+      { 
+        isLoading ? <CircularProgress /> : 
+        (
+          <>
+            <Container maxWidth="sm">
+              <Typography component="div" className="App">
+                {/* Header Section Start */}
+                <div className="App__Header">
+                  <Header
+                    link={data.link}
+                    logo={data.logo}
+                  >
+                    <LanguageToggle 
+                      language={language}
+                      setLanguage={setLanguage}
+                    />
+                  </Header>
+                </div>
+                {/* Header Section End */}
+                {/* Content Section Start*/}
+                <div className="App__ImageHolder">
+                  <ImageHolder src={data.image} />
+                </div>
+                <div className="App__Grid">
+                  <Grid container spacing={0}>
+                    <Grid item xs={4}>
+                      Grid1Grid1Grid1Grid1Grid1Grid1Grid1Grid1
+                    </Grid>
+                    <Grid item xs={4}>
+                      Grid1
+                    </Grid>
+                    <Grid item xs={4} style={{ textAlign: "center" }}>
+                      <Button variant="outlined" color="primary">
+                        Primary
+                      </Button>
+                    </Grid>
+                  </Grid>
+                  <div>...</div>
+                  <Grid container spacing={0}>
+                    <Grid item xs={4}>
+                      Grid1
+                    </Grid>
+                    <Grid item xs={4}>
+                      Grid1
+                    </Grid>
+                    <Grid item xs={4} style={{ textAlign: "center" }}>
+                      <Button variant="contained" color="primary">
+                        Primary
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+                <div className="App__Button">
+                  <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ minWidth: "100%" }}
+                      >
+                        Primary
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+                <div className="App__Button">
+                  <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ minWidth: "100%" }}
+                      >
+                        Primary
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+                <div className="App__Button">
+                  <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ minWidth: "100%" }}
+                      >
+                        Primary
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+                {/* Content Section End*/}
+                {/* Footer Section Start */}
+                <div className="App__Footer">
+                  <Footer />
+                </div>
+                {/* Footer Section End */}
+              </Typography>
+            </Container>
+          </>
+        )
+      }
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const theme = (colorPalette) => createMuiTheme({
+  palette: {
+    primary: {
+      main: colorPalette.primaryColor,
+      contrastText: colorPalette.contrastText
+    }
+  }
+});
+
+export default theme;
