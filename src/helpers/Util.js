718028db00a0844f4eb4be775ad318baf90bfa78
Sathyesh <sathyesh.fns@gmail.com>
2020-10-02 12:27:06 +0200

    fix: updated date logic to show the right weekday

--- src/helpers/Util.js
+++ src/helpers/Util.js
@@ -7,17 +7,17 @@ export const getTranslatedDateForIndex = (index, languageData) => {
   } else if(index === 1){
     return languageData.date.tomorrow || "Tomorrow";
   } else {
-    const givenDate = moment().add(index+1, 'days');
-    const weekDay = givenDate.isoWeekday();
-    const weekDayText = languageData.date.weekdays[weekDay-1] || null;  
+    const givenDate = moment().add(index, 'days');
+    const weekDay = (givenDate.isoWeekday() === 7)?0:givenDate.isoWeekday();
+    const weekDayText = languageData.date.weekdays[weekDay] || "";  
     return weekDayText+" "+givenDate.format(' DD.MM.YY') || givenDate.format('DD.MM.YY');
   }
 };
 
 export const getTranslatedDateForDate = (date, languageData) => {
     const momentDate = moment(date, 'YYYY-MM-DD');
-    const weekDay = momentDate.isoWeekday();
-    const weekDayText = languageData.date.weekdays[weekDay-1] || null;
+    const weekDay = (momentDate.isoWeekday() === 7)?0:momentDate.isoWeekday();
+    const weekDayText = languageData.date.weekdays[weekDay] || "";
     return weekDayText+" "+momentDate.format(' DD.MM.YY') || momentDate.format('DD.MM.YY');;
 };
 
