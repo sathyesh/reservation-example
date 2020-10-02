cacea08fa931663cde755019535401fec519b778
Sathyesh <sathyesh.fns@gmail.com>
2020-10-02 12:10:39 +0200

    fix: updated date logic to show date correctly

--- src/helpers/Util.js
+++ src/helpers/Util.js
@@ -1,14 +1,15 @@
 const moment = require('moment');
 
 export const getTranslatedDateForIndex = (index, languageData) => {
-  const givenDate = moment().add(index, 'days');
-  const weekDay = givenDate.isoWeekday();
-  const weekDayText = languageData.date.weekdays[weekDay-1] || null;
+  
   if(index === 0){
     return languageData.date.today || "Today";
   } else if(index === 1){
     return languageData.date.tomorrow || "Tomorrow";
-  } else {  
+  } else {
+    const givenDate = moment().add(index+1, 'days');
+    const weekDay = givenDate.isoWeekday();
+    const weekDayText = languageData.date.weekdays[weekDay-1] || null;  
     return weekDayText+" "+givenDate.format(' DD.MM.YY') || givenDate.format('DD.MM.YY');
   }
 };
